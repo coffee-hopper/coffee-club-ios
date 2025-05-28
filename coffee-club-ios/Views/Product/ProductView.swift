458a701f714b35637711b0fa9bfd451dc94cbfa2
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-05-28 11:40:17 +0300

    main layout/mock version done

---
+++ coffee-club-ios/Views/Product/ProductView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductView.swift
+//  coffee-club-ios
+//
+//  Created by Bahadır Pekcan on 23.05.2025.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductView()
+}
