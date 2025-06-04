70f56f6ea0c889d4c9f93001f7b76c3f08837ed5
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-06-04 12:21:41 +0300

    ProductCard added & Fetch :done on productListView and ProductCard Header - user profile fetch : done, Profile View added

---
+++ coffee-club-ios/Views/Layout/MainHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MainHeaderView.swift
+//  coffee-club-ios
+//
+//  Created by Bahadır Pekcan on 30.05.2025.
+//
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainHeaderView()
+}
---
+++ coffee-club-ios/Views/Product/ProductCard.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct ProductCardView: View {
+    let product: Product
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Image(product.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 80)
+                .cornerRadius(8)
+
+            HStack {
+                Text(product.name)
+                    .font(.subheadline)
+                    .lineLimit(1)
+                    .foregroundColor(.primary)
+
+                Spacer()
+
+                Text("\(product.price)₺")
+                    .font(.subheadline.bold())
+                    .foregroundColor(.green)
+            }
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 12).stroke(Color.gray.opacity(0.4), lineWidth: 1)
+        )
+        .frame(width: 140, height: 180)
+    }
+}
